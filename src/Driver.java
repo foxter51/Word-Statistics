b0d51e32ecd939738cc343e059928b813d42f9df
Ihor Fedorchenko <ihorgood0@gmail.com>
2022-10-27 21:01:26 +0300

    driver defined

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        System.out.print("Set text: ");
+        String text = in.nextLine();
+
+        System.out.print("Set history: ");
+        String history = in.nextLine();
+
+        WordStatistics wordStatistics = new WordStatistics(text, history);
+
+        System.out.print("Enter the initial number of words: ");
+        int wordsNumber = in.nextInt();
+
+        System.out.println("Word Counts:");
+
+        String[][] initWords = wordStatistics.countInitWords(wordsNumber);
+        for (int i = 0; i < initWords[0].length; i++) {
+            System.out.println(initWords[i][0] + " " + initWords[i][1]);
+        }
+
+        wordStatistics.longestSentence();
+
+        System.out.println("Age of text: " +wordStatistics.textAge());
     }
 }
